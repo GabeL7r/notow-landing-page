bdae60c2ebc8d9ad93f8191dd75a3e725ddf1e5c
Chris McDermut <chrismcdermut@gmail.com>
2017-12-19 22:00:02 -0800

    Add-monitoring-and-testing to PIAB

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Raven from 'raven-js'
+Raven.config('https://05de4eb56d71407b838cf481f7f28939@sentry.io/260297').install()
 import { render } from 'react-dom';
 
 import '../css/style.css';
 
 import keenImage from '../assets/keen.png';
 
-console.log('Hello World!');
+// console.log(hello);
 
 
 export default class Hello extends Component {
