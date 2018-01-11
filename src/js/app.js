b90fdbd0fcc5f773278ce44fdf1a7839a4561f7d
Chris McDermut <chrismcdermut@gmail.com>
2018-01-10 23:15:10 -0800

    PIAB-13 adding precommit ffor eslint, css and config files (#14)

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import Raven from 'raven-js'
-Raven.config('https://05de4eb56d71407b838cf481f7f28939@sentry.io/260297').install()
-import { render } from 'react-dom';
-
+// import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { render } from 'react-dom';
+import Raven from 'raven-js';
 import '../css/style.css';
-
 import keenImage from '../assets/keen.png';
 
-// console.log(hello);
+Raven.config('https://05de4eb56d71407b838cf481f7f28939@sentry.io/260297').install();
 
+/* saving for posterity in case we need to go back to stateful
+// export default class Hello extends Component {
+//   render() {
+//     return (
+//       <div>
+//         Hello from react
+//         {
+//
+//         }
+//         <img src={keenImage} alt="Commander Keen" />
+//       </div>
+//     );
+//   }
+// }
+*/
 
-export default class Hello extends Component {
-  render() {
-    return (
-      <div>
-        Hello from react
+// testing pre-push
 
-        {
+const Hello = (
+  <div>
+    Hello from react
+    {}
+    <img src={keenImage} alt="Commander Keen" />
+  </div>
+);
 
-        }
-        <img src={ keenImage } alt='Commander Keen' />
-      </div>
-    );
-  }
-}
+export default Hello;
 
-render(<Hello />, document.getElementById('app'));
+ReactDOM.render(<Hello />, document.getElementById('app'));
