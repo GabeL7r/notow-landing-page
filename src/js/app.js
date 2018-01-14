e6eed276d4a0e245dafde53f8d3ac4d34f9c5b06
Chris McDermut <chrismcdermut@gmail.com>
2018-01-13 16:45:00 -0800

    PIAB-12 docs, adding commitizent to standardize commit messages and enable hooks and triggers (#15)
    PIAB-12 docs, adding commitizen

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,51 @@
-// import React, { Component } from 'react';
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import { render } from 'react-dom';
+//  below is for stateful
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+//  above is for stateful
+
+//  below is stateless
+//  import React from 'react';
+//  import ReactDOM from 'react-dom';
+//  above is stateless
+
 import Raven from 'raven-js';
 import '../css/style.css';
 import keenImage from '../assets/keen.png';
 
 Raven.config('https://05de4eb56d71407b838cf481f7f28939@sentry.io/260297').install();
 
-/* saving for posterity in case we need to go back to stateful
-// export default class Hello extends Component {
-//   render() {
-//     return (
-//       <div>
-//         Hello from react
-//         {
+//  below saving for posterity in case we need to go back to stateful
+export default class Hello extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Hello from react
+        {
+
+        }
+        <img src={keenImage} alt="Commander Keen" />
+      </div>
+    );
+  }
+}
+//  above saving for posterity in case we need to go back to stateful
+
+render(<Hello />, document.getElementById('app'));
+
+// const Hello = (
+//   <div>
+//     Hello from react
+//     {}
+//     <img src={keenImage} alt="Commander Keen" />
+//   </div>
+// );
 //
-//         }
-//         <img src={keenImage} alt="Commander Keen" />
-//       </div>
-//     );
-//   }
-// }
-*/
-
-// testing pre-push
-
-const Hello = (
-  <div>
-    Hello from react
-    {}
-    <img src={keenImage} alt="Commander Keen" />
-  </div>
-);
-
-export default Hello;
-
-ReactDOM.render(<Hello />, document.getElementById('app'));
+// export default Hello;
+//
+// ReactDOM.render(<Hello />, document.getElementById('app'));
